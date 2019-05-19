94a57037a6a91f7ed1182eccbac815843a287c14
Arslan <1041238415@qq.com>
2019-05-19 23:45:06 +0800

    init

---
+++ src/router/router.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import Router from "vue-router";
+import store from "../store/store";
+import Home from "../views/Home.vue";
+
+Vue.use(Router);
+
+/**
+ * @param {String} name 文件夹名称
+ * @param {String} component 视图组件
+ * 
+ */
+
+ const getComponent =(name,component)=>()=>import(`@/views/${name}/${component}.vue`);
+
+const myRouter= new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      redirect:'/home',
+      component: getComponent('login','index')
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: getComponent('login','index')
+    },
+    {
+      path:"/",
+      component:getComponent('layout','Layout'),
+      children:[{
+        path:'/home',
+        name:'home',
+        component:getComponent('home','index'),
+        meta:{title:'首页'}
+      }]
+    },
+    {
+      path:'/icon',
+      component:getComponent('icons','index'),
+      name:'icon',
+      meta:{title:'自定义图标'},
+    }
+  ]
+});
+
+export default myRouter
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        基本布局
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
